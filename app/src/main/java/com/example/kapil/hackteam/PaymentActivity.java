321cffd04b4a3331d32bd954918ffe8b2e211799
kapilbindal <kapil.bindal98@gmail.com>
2018-05-26 03:26:46 +0530

    Add money feature in payment page added

--- app/src/main/java/com/example/kapil/hackteam/PaymentActivity.java
+++ app/src/main/java/com/example/kapil/hackteam/PaymentActivity.java
@@ -1,20 +1,18 @@
 package com.example.kapil.hackteam;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PointF;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 import com.example.kapil.hackteam.adapter.OrderRecyclerAdapter;
@@ -30,15 +28,12 @@ import java.util.Date;
 
 public class PaymentActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener {
 
-    public static String orderNumber = "52170109";
-    private TextView detailsTextView,tvAmount;
-    public static final String TAG = "fdf";
-    private QRCodeReaderView qrCodeReaderView;
-    LinearLayout paymentLayout;
-    EditText edSuccess;
-    ArrayList<Order> orderArrayList;
-    Button btnPaymentAmount;
-    OrderRecyclerAdapter orderRecyclerAdapter;
+    TextView detailsTextView,tvAddMoneyToWallet;
+    EditText edAmount,edBankName,edAddAmount;
+    QRCodeReaderView qrCodeReaderView;
+    LinearLayout paymentLayout,paymenLayoutQR,addMoneyLayout;
+    EditText edSuccess,edAddSuccess;
+    Button btnPaymentAmount,btnAddMoney;
     DatabaseReference mDatabase,mDatabaseSummary;
     ProgressDialog progressDialog;
 
@@ -47,15 +42,61 @@ public class PaymentActivity extends AppCompatActivity implements QRCodeReaderVi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
+        btnAddMoney = findViewById(R.id.btnAddMoney);
+        edBankName = findViewById(R.id.edBankName);
+        edAddAmount = findViewById(R.id.edAddAmount);
+        tvAddMoneyToWallet = findViewById(R.id.tvAddMoneyToWallet);
         detailsTextView = findViewById(R.id.detailsTextView);
         paymentLayout = findViewById(R.id.paymentLayout);
+        paymenLayoutQR = findViewById(R.id.paymentLayoutQR);
+        addMoneyLayout = findViewById(R.id.addMoneyLayout);
         btnPaymentAmount = findViewById(R.id.btnPayAmount);
         mDatabase = FirebaseDatabase.getInstance().getReference("Orders");
         mDatabaseSummary = FirebaseDatabase.getInstance().getReference("Summary");
-        tvAmount = findViewById(R.id.tvAmount);
+        edAmount = findViewById(R.id.edAmount);
         edSuccess = findViewById(R.id.edSuccess);
+        edAddSuccess = findViewById(R.id.edAddSuccess);
         progressDialog = new ProgressDialog(this);
 
+        tvAddMoneyToWallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                paymenLayoutQR.setVisibility(View.GONE);
+                addMoneyLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnAddMoney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = mDatabaseSummary.push().getKey();
+                String currentTimeString = DateFormat.getTimeInstance().format(new Date());
+                String currentDateString = DateFormat.getDateInstance().format(new Date());
+                Summary summary = new Summary("From: " + edBankName.getText(),currentTimeString,currentDateString,Integer.parseInt(edAddAmount.getText().toString())  );
+                mDatabaseSummary.child(id).setValue(summary);
+                btnAddMoney.setVisibility(View.GONE);
+
+                progressDialog.setMessage("Please wait your money is being Added");
+                progressDialog.show();
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.hide();
+                        edAddSuccess.setVisibility(View.VISIBLE);
+                    }
+                }, 2000);
+
+                Handler handler3 = new Handler();
+                handler3.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(PaymentActivity.this,MainActivity.class));
+                    }
+                }, 4000);
+            }
+        });
 
         btnPaymentAmount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,10 +105,10 @@ public class PaymentActivity extends AppCompatActivity implements QRCodeReaderVi
                 //Toast.makeText(PaymentActivity.this, detailsTextView.getText().toString(), Toast.LENGTH_SHORT).show();
                 String currentTimeString = DateFormat.getTimeInstance().format(new Date());
                 String currentDateString = DateFormat.getDateInstance().format(new Date());
-                Summary summary = new Summary("From: " + id,currentTimeString,currentDateString,Integer.parseInt(tvAmount.getText().toString())  );
-                Order order = new Order(id ,currentTimeString,currentDateString,detailsTextView.getText().toString(),Integer.parseInt(tvAmount.getText().toString()));
+                //Summary summary = new Summary("From: " + id,currentTimeString,currentDateString,Integer.parseInt(edAmount.getText().toString())  );
+                Order order = new Order(id ,currentTimeString,currentDateString,detailsTextView.getText().toString(),Integer.parseInt(edAmount.getText().toString()));
                 mDatabase.child(id).setValue(order);
-                mDatabaseSummary.child(id).setValue(summary);
+                //mDatabaseSummary.child(id).setValue(summary);
                 btnPaymentAmount.setVisibility(View.GONE);
 
                 progressDialog.setMessage("Please wait your transaction is being processed");
@@ -113,7 +154,7 @@ public class PaymentActivity extends AppCompatActivity implements QRCodeReaderVi
 
     @Override
     public void onQRCodeRead(String text, PointF[] points) {
-        qrCodeReaderView.setVisibility(View.GONE);
+        paymenLayoutQR.setVisibility(View.GONE);
         paymentLayout.setVisibility(View.VISIBLE);
         detailsTextView.setText(text);
     }
